85a6aa019189b22160c9a67b88486c06f9256d90
Jose <jose@oldcutler.club>
2023-08-29 10:26:57 -0400

    create home & fetch-wrapper

---
+++ ghi/src/fetch-wrapper.js
@@ -0,0 +1,51 @@
+export class FetchWrapper {
+    constructor(baseURL) {
+        this.baseURL = baseURL;
+    }
+
+    async get(endpoint) {
+        try {
+            const response = await fetch(this.baseURL + endpoint)
+            if (response.ok) {
+                const data = await response.json()
+                return data
+            } else {
+                throw new Error('Response not "OK"')
+            }
+        } catch (error) {
+            console.error('error', error)
+        }
+    }
+
+    async put(endpoint, body) {
+        return this._send("PUT", endpoint, body)
+    }
+
+    async post(endpoint, body) {
+        return this._send("POST", endpoint, body)
+    }
+
+    async delete(endpoint, body) {
+        return this._send("DELETE", endpoint, body)
+    }
+
+    async _send(method, endpoint, body) {
+        try {
+            const response = await fetch(this.baseURL + endpoint, {
+                method: method,
+                body: JSON.stringify(body),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            if (response.ok) {
+                const data = await response.json()
+                return data
+            } else {
+                throw new Error('Response not "OK"')
+            }
+        } catch (error) {
+            console.error('error', error)
+        }
+    }
+}
