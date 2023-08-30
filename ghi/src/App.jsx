2d58bb1959d45b24c5b4270730a1f9d59271b14c
Jose <jose@oldcutler.club>
2023-08-30 16:02:33 -0400

    created SignUp and login forms

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-
+import { AuthProvider } from '@galvanize-inc/jwtdown-for-react'
+import LoginForm from './LoginForm'
+import SignupForm from './SignupForm'
 import Home from "./home";
 import PlanList from "./PlansList";
 // import Login from "./Login";
@@ -15,24 +17,50 @@ import PlanList from "./PlansList";
 // import ExpenseCreate from "./ExpenseCreate";
 
 function App() {
+  const baseUrl = "http://localhost:8000"
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<Logout />} /> */}
-        <Route path="/plans" element={<PlanList />} />
-        {/* <Route path="/plan/{id}" element={<PlanDetail />} />
-        <Route path="/plan/create" element={<PlanCreate />} />
-        <Route path="/plan{id}/edit" element={<PlanEdit />} />
-        <Route path="/journal" element={<Journal />} />
-        <Route path="/journal/{id}" element={<JournalDetail />} />
-        <Route path="/journal/create" element={<JournalCreate />} />
-        <Route path="/journal/{id}/edit" element={<JournalEdit />} />
-        <Route path="/expense/create" element={<ExpenseCreate />} /> */}
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider baseUrl={baseUrl}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Signup" element={<SignupForm />} />
+          <Route path="/Login" element={<LoginForm />} />
+          <Route path="/plans" element={<PlanList />} />
+          {/* <Route path="/plan/{id}" element={<PlanDetail />} />
+          <Route path="/plan/create" element={<PlanCreate />} />
+          <Route path="/plan{id}/edit" element={<PlanEdit />} />
+          <Route path="/journal" element={<Journal />} />
+          <Route path="/journal/{id}" element={<JournalDetail />} />
+          <Route path="/journal/create" element={<JournalCreate />} />
+          <Route path="/journal/{id}/edit" element={<JournalEdit />} />
+          <Route path="/expense/create" element={<ExpenseCreate />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
 export default App;
+
+
+// import './App.css'
+// import { AuthProvider } from '@galvanize-inc/jwtdown-for-react'
+// import LoginForm from './LoginForm'
+// import SignupForm from './SignupForm'
+// import { Sign } from "crypto";
+
+// function App() {
+//   const baseUrl = "http://localhost:8000"
+
+//   return (
+//     <>
+//       <AuthProvider baseUrl={baseUrl}>
+//         <SignupForm />
+//         <LoginForm />
+//       </AuthProvider>
+//     </>
+//  )
+// }
+
+// export default App
