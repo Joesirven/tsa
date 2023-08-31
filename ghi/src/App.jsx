734d8a840f6390a2df647c9dda20fd45f80e8f4b
Britley Desir <bdesir@protonmail.com>
2023-08-31 15:51:44 -0400

    plan detail

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -2,10 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import Home from "./home";
-import PlanList from "./PlansList";
+// import PlanList from "./PlansList";
 // import Login from "./Login";
 // import Logout from "./Logout";
-// import PlanDetail from "./PlanDetail";
+import PlanDetail from "./PlanDetail";
 // import PlanCreate from "./PlanCreate";
 // import PlanEdit from "./PlanEdit";
 // import Journal from "./Journal";
@@ -20,10 +20,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/login" element={<Login />} />
-        <Route path="/logout" element={<Logout />} /> */}
-        <Route path="/plans" element={<PlanList />} />
-        {/* <Route path="/plan/{id}" element={<PlanDetail />} />
-        <Route path="/plan/create" element={<PlanCreate />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/plans" element={<PlanList />} /> */}
+        <Route path="/plan/{id}" element={<PlanDetail />} />
+        {/* <Route path="/plan/create" element={<PlanCreate />} />
         <Route path="/plan{id}/edit" element={<PlanEdit />} />
         <Route path="/journal" element={<Journal />} />
         <Route path="/journal/{id}" element={<JournalDetail />} />
