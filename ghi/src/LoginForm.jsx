91e73a0b4e63cbe5bed654021161de5791d8d27f
Jose <jose@oldcutler.club>
2023-09-06 19:02:03 -0400

    Nav bar and login and signup display messages

--- ghi/src/LoginForm.jsx
+++ ghi/src/LoginForm.jsx
@@ -1,25 +1,63 @@
 import useToken from "@galvanize-inc/jwtdown-for-react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Alert from "react-bootstrap/Alert";
 
 const LoginForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const { login } = useToken();
+  // const [displayMessage, setMessage] = useState("alert alert-success d-none");
+  const { login, token } = useToken();
+  const navigate = useNavigate();
+  const [click, setClick] = useState(false);
+  const [error, setError] = useState(false);
+  const [message, setMessage] = useState("");
+  const [messageType, setMessageType] = useState("");
 
-  const handleSubmit = (e) => {
+  useEffect(() => {
+    if (token) {
+      navigate("/plans");
+      setError(false);
+      setMessage("Login Successful");
+      setMessageType("success");
+    }
+    if (!token && click) {
+      setTimeout(function () {
+        setError(true);
+        setMessage("Password and Username do not match");
+        setMessageType("danger");
+      }, 2000);
+    }
+  }, [token, click, navigate]);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(`username: ${username} password: ${password}`);
-    login(username, password);
-    e.target.reset();
+
+    if (!username || !password) {
+      setError(true);
+      setMessage("Password and Username cannot be blank.");
+      setMessageType("danger");
+      return
+    }
+
+    try {
+      setClick(true);
+      await login(username, password);
+    } catch (error) {
+      setError(true);
+      setMessage("Cannot login. Try again.");
+      setMessageType("danger");
+    }
   };
 
   return (
     <div className="card text-bg-light mb-3">
       <h5 className="card-header">Login</h5>
       <div className="card-body">
+        {message && <Alert variant={messageType}>{message}</Alert>}
         <form onSubmit={(e) => handleSubmit(e)}>
           <div className="mb-3">
-            <label className="form-label">Username:</label>
+            <label className="form-label">Email:</label>
             <input
               name="username"
               type="text"
--- ghi/src/nav.jsx
+++ ghi/src/nav.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import useToken from '@galvanize-inc/jwtdown-for-react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 function NavComponent() {
-  const { token, logout, fetchWithToken } = useToken();
+  const { token, logout, } = useToken();
+  // const navigate = useNavigate();
 
   function handleLogout() {
     logout();
-    fetchWithToken('/logout');
+    // navigate("/");
   }
 
   return (
@@ -21,7 +23,7 @@ function NavComponent() {
             <>
               <Nav.Link as={NavLink} to="/" exact>Home</Nav.Link>
               <NavDropdown title="Travel Plans" id="basic-nav-dropdown">
-                <NavDropdown.Item as={NavLink} to="/plans">Your Plans</NavDropdown.Item>
+                <NavDropdown.Item as={NavLink} to="/plans">My Plans</NavDropdown.Item>
                 <NavDropdown.Item as={NavLink} to="/plan/create">New Plan</NavDropdown.Item>
                 <NavDropdown.Item as={NavLink} to="/expense/create">New Expense</NavDropdown.Item>
               </NavDropdown>
@@ -42,64 +44,5 @@ function NavComponent() {
     </Navbar>
   );
 }
-
-
-
-// function Nav() {
-//   const { token } = useToken();
-
-//   return (
-//     <nav className="navbar navbar-expand-lg navbar-dark bg-success">
-//       <div className="container-fluid">
-//         <NavLink className="navbar-brand" to="/">TSA</NavLink>
-//         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-//           <span className="navbar-toggler-icon"></span>
-//         </button>
-//         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-//           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-//             {token ? (
-//               <>
-//                 <li className="nav-item">
-//                   <NavLink className="nav-link" aria-current="page" to="/" exact>Home</NavLink>
-//                 </li>
-//                 <li className="nav-item dropdown">
-//                   <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-//                     Travel Plans
-//                   </a>
-//                   <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-//                     <li><NavLink className="dropdown-item" to="/plans">Your Plans</NavLink></li>
-//                     <li><NavLink className="dropdown-item" to="/plan/create">New Plan</NavLink></li>
-//                     <li><NavLink className="dropdown-item" to="/expense/create">New Expense</NavLink></li>
-//                   </ul>
-//                 </li>
-//                 <li className="nav-item dropdown">
-//                   <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-//                     Journal
-//                   </a>
-//                   <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-//                     <li><NavLink className="dropdown-item" to="/journal">My Journal</NavLink></li>
-//                     <li><NavLink className="dropdown-item" to="/journal/create">Add Manufacturer</NavLink></li>
-//                   </ul>
-//                 </li>
-//                 <li className="nav-item">
-//                   <NavLink className="nav-link" to="/logout">Log Out</NavLink>
-//                 </li>
-//               </>
-//             ) : (
-//               <>
-//                 <li className="nav-item">
-//                   <NavLink className="nav-link" to="/Login">Login</NavLink>
-//                 </li>
-//                 <li className="nav-item">
-//                   <NavLink className="nav-link" to="/Signup">Sign Up</NavLink>
-//                 </li>
-//               </>
-//             )}
-//           </ul>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// }
 
 export default NavComponent;
