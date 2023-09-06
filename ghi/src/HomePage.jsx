fd2f30eaf2f4452c7971b703f1df5e86086ae216
Dom Cross <dominickdcross@gmail.com>
2023-09-06 19:19:45 -0400

    merge

--- ghi/src/HomePage.jsx
+++ ghi/src/HomePage.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 export default function HomePage() {
   return (
     <div>
       <h1>Home Page Place Holder</h1>
       <div className="container">
-        <Link to="/Login">Login</Link>
-        <Link to="/Signup">Signup</Link>
-        <Link to="/plans">PlanList</Link>
-        <Link to="/plan/create">PlanCreate</Link>
       </div>
     </div>
   );
