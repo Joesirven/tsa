9f61b137ab905b601b28c65aa06eef2373e5076a
Britley Desir <bdesir@protonmail.com>
2023-09-11 11:46:34 -0400

    changed unit tests

--- api/tests/test_savings.py
+++ api/tests/test_savings.py
@@ -1,30 +1,57 @@
-from fastapi.testclient import TestClient
-from main import app
-from queries.users import UserRepository, UserOutWithPassword
-from pydantic import BaseModel
+import pytest
+import asyncio
+from unittest.mock import patch, Mock
+from fastapi import Response
+from queries.savings import SavingsRepository, SavingsIn, SavingsOut
+from routers.savings import create_savings
 
-client = TestClient(app)
+@pytest.mark.asyncio
+class TestCreateSavingsEndpoint:
 
-class MockUserOutWithPassword(BaseModel):
-  id: int
-  first_name: str
-  last_name: str
-  email: str
-  hashed_password: str
+    @patch('routers.savings.SavingsRepository')
+    @patch('routers.savings.authenticator.get_current_account_data')
+    @patch('routers.savings.Response')
+    async def test_create_savings_success(self, mock_response, mock_get_current_account_data, mock_savings_repository):
+        mock_repo_instance = mock_savings_repository.return_value
+        mock_get_current_account_data.return_value = {"user_id": 1}
+        mock_response_instance = mock_response.return_value
+        mock_response_instance.status_code = 200
 
-class MockUserRepository(BaseModel):
-  def get_all(self):
-    return MockUserOutWithPassword(
-      id =  1,
-      first_name = "britlee",
-      last_name = "britlee",
-      email =  "britlee",
-      hashed_password = "$2b$12$i4aMPNEXd84/0uPx8MtPQOw/xzE5zDtmSOAsEYSq2uNzeI2b8k79O"
-    )
+        savings_input = SavingsIn(
+            current_amount_saved=0.00,
+            final_goal_amount=5000.00,
+            plans_id=1
+        )
 
-def test_get_all_users():
-  app.dependency_overrides[UserRepository] = MockUserRepository
-  response = client.get("/users")
-  assert response.status_code == 200
-  app.dependency_overrides = {}
-  
+        expected_savings_output = SavingsOut(id=1, **savings_input.dict())
+        mock_repo_instance.create.return_value = expected_savings_output
+
+        created_savings = await create_savings(savings_input, mock_response_instance, mock_repo_instance)
+
+        assert created_savings == expected_savings_output
+        mock_repo_instance.create.assert_called_once_with(savings_input)
+        mock_response_instance.status_code = 200
+
+    @patch('routers.savings.SavingsRepository')
+    @patch('routers.savings.authenticator.get_current_account_data')
+    @patch('routers.savings.Response')
+    async def test_create_savings_failure(self, mock_response, mock_get_current_account_data, mock_savings_repository):
+        mock_repo_instance = mock_savings_repository.return_value
+        mock_get_current_account_data.return_value = {"user_id": 1}
+        mock_response_instance = mock_response.return_value
+        mock_response_instance.status_code = 500
+
+        savings_input = SavingsIn(
+            current_amount_saved=0.00,
+            final_goal_amount=5000.00,
+            plans_id=1
+        )
+
+        expected_error = {"message": "Could not create savings."}
+        mock_repo_instance.create.return_value = expected_error
+
+        created_savings = await create_savings(savings_input, mock_response_instance, mock_repo_instance)
+
+        assert created_savings == expected_error
+        mock_repo_instance.create.assert_called_once_with(savings_input)
+        mock_response_instance.status_code = 500
