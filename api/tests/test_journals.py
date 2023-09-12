8f4ac786cc4f820b0584d54c3379667dc3c1ac76
Britley Desir <bdesir@protonmail.com>
2023-09-12 18:09:30 -0400

    fixed unit test

--- api/tests/test_journals.py
+++ api/tests/test_journals.py
@@ -9,11 +9,15 @@ from unittest.mock import AsyncMock
 
 @pytest.mark.asyncio
 class TestCreateJournalEndpoint:
-
-    @patch('routers.journals.JournalRepository')
-    @patch('routers.journals.authenticator.get_current_account_data')
-    @patch('routers.journals.Response')
-    async def test_create_journal_success(self, mock_response, mock_get_current_account_data, mock_journal_repository):
+    @patch("routers.journals.JournalRepository")
+    @patch("routers.journals.authenticator.get_current_account_data")
+    @patch("routers.journals.Response")
+    async def test_create_journal_success(
+        self,
+        mock_response,
+        mock_get_current_account_data,
+        mock_journal_repository,
+    ):
         mock_repo_instance = mock_journal_repository.return_value
         mock_get_current_account_data.return_value = {"user_id": 1}
         mock_response_instance = mock_response.return_value
@@ -24,21 +28,28 @@ class TestCreateJournalEndpoint:
             description="This is a sample journal content.",
             rating=5,
             date="2023-01-01",
-            users_id=1
+            users_id=1,
         )
 
         expected_journal_output = JournalOut(id=1, **journal_input.dict())
         mock_repo_instance.create.return_value = expected_journal_output
-        mock_response_instance.create.return_value = expected_journal_output  # Mocking the return value
+        mock_response_instance.create.return_value = expected_journal_output
 
-        created_journal = await create_journal(journal_input, mock_response_instance, mock_repo_instance)
+        created_journal = await create_journal(
+            journal_input, mock_response_instance, mock_repo_instance
+        )
 
         assert created_journal == expected_journal_output
 
-    @patch('routers.journals.JournalRepository')
-    @patch('routers.journals.authenticator.get_current_account_data')
-    @patch('routers.journals.Response')
-    async def test_create_journal_failure(self, mock_response, mock_get_current_account_data, mock_journal_repository):
+    @patch("routers.journals.JournalRepository")
+    @patch("routers.journals.authenticator.get_current_account_data")
+    @patch("routers.journals.Response")
+    async def test_create_journal_failure(
+        self,
+        mock_response,
+        mock_get_current_account_data,
+        mock_journal_repository,
+    ):
         mock_repo_instance = mock_journal_repository.return_value
         mock_get_current_account_data.return_value = {"user_id": 1}
         mock_response_instance = mock_response.return_value
@@ -49,13 +60,15 @@ class TestCreateJournalEndpoint:
             description="This is a sample journal content.",
             rating=5,
             date="2023-01-01",
-            users_id=1
+            users_id=1,
         )
 
         expected_error = {"message": "Could not create a journal entry"}
         mock_repo_instance.create.return_value = expected_error
-        mock_response_instance.create.return_value = expected_error  # Mocking the return value
+        mock_response_instance.create.return_value = expected_error
 
-        created_journal = await create_journal(journal_input, mock_response_instance, mock_repo_instance)
+        created_journal = await create_journal(
+            journal_input, mock_response_instance, mock_repo_instance
+        )
 
         assert created_journal == expected_error
